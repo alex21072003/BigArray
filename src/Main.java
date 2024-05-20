cc66698b38f2dd701badbf0136e32cd3caa0ff37
alex21072003 <alexkarpinskiy21@gmail.com>
2024-05-20 11:09:43 +0300

    Initial commit My array

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a [] = {12,33,44,55};
+        System.out.print("How much do you have scarples");
+        System.out.println("how much do you have numbers");
+    }
+}
